45bb0369cc8ddd2871d58d9c509b99240bc4cd0c
akashprakash01 <akash700638@gmail.com>
2026-02-07 17:28:59 +0530

    Add

--- AssignmentOne/HospitalManagementSystem/HospitalManagement/Models/Doctor.cs
+++ AssignmentOne/HospitalManagementSystem/HospitalManagement/Models/Doctor.cs
@@ -15,7 +15,7 @@ namespace HospitalManagement.Models
         [Range(18, 60, ErrorMessage = "Age must between 18 and 60")]
         public int Age { get; set; }
 
-        [Required(ErrorMessage = "Name should not be empty")]
+        [Required(ErrorMessage = "Specialization should not be empty")]
         [RegularExpression(@"^[A-Za-z][A-Za-z\s]*$",
           ErrorMessage = "Name must start with a letter and contain only alphabets and spaces")]
         public string Specialization { get; set; }
